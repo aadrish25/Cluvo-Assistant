4c959365e9a72c40e3c493ec938d400a80ec393c
aadrish25 <aadrishsen.25@gmail.com>
2026-08-30 18:27:27 +0530

    ..

--- backend/orchestrator/llm.py
+++ backend/orchestrator/llm.py
@@ -1,5 +1,4 @@
 from agno.models.ollama import Ollama
-from backend.config import OLLAMA_API_KEY
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,7 +6,7 @@ load_dotenv()
 
 class Gemma4_31B:
     def __init__(self):
-        self.model = Ollama(id="gemma4:31b-cloud",api_key=OLLAMA_API_KEY)
+        self.model = Ollama(id="gemma4:31b-cloud")
         
     def get_model(self):
         return self.model
